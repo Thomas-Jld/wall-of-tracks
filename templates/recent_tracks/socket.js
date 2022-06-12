eb59ded0048c586c3dd43d2fa7e03bfe26cb2185
Thomas <thomas.juldo@edu.devinci.fr>
2022-06-12 15:17:36 +0200

    Ignoring duplicates

--- templates/recent_tracks/socket.js
+++ templates/recent_tracks/socket.js
@@ -21,10 +21,10 @@ let current_timestamp;
 
 socket.on("redirect", (url) => {
     window.location.href = url;
-})
+});
 
 socket.on("recent", (data) => {
-    if(last_played == undefined || last_played != data[0].played_at) {
+    if (last_played == undefined || last_played != data[0].played_at) {
         let name, artist, album, image;
         name = data[0].track.name;
         artist = data[0].track.artists[0].name;
@@ -33,12 +33,12 @@ socket.on("recent", (data) => {
         last_played = data[0].played_at;
         loadImage(image, (img) => {
             update_recently_played(img, name, artist, album);
-        })
+        });
     }
 });
 
 socket.on("playing", (data) => {
-    if(current_timestamp == undefined || current_timestamp != data.timestamp) {
+    if (current_timestamp == undefined || current_timestamp != data.timestamp) {
         let name, artist, album, image;
         name = data.item.name;
         artist = data.item.artists[0].name;
@@ -50,41 +50,44 @@ socket.on("playing", (data) => {
         });
     }
     is_playing = data.is_playing;
-})
+});
 
 socket.on("init_recent", async (data) => {
-    let amount = min(data.length, 48);
+    let amount = data.length;
     let name, artist, album, image;
-    for(let i = 0; i < amount; i++) {
-        name = data[amount-i-1].track.name;
-        artist = data[amount-i-1].track.artists[0].name;
-        album = data[amount-i-1].track.album.name;
-        image = data[amount-i-1].track.album.images[0].url;
+    for (let i = 0; i < amount; i++) {
+        if (
+            name == data[amount - i - 1].track.name &&
+            album == data[amount - i - 1].track.album.name
+        )
+            continue;
+        name = data[amount - i - 1].track.name;
+        artist = data[amount - i - 1].track.artists[0].name;
+        album = data[amount - i - 1].track.album.name;
+        image = data[amount - i - 1].track.album.images[0].url;
         loadImage(image, (img) => {
             update_recently_played(img, name, artist, album);
         });
-        while(!img_loaded) {
-            await sleep(100);
+        while (!img_loaded) {
+            await sleep(50);
         }
         img_loaded = false;
     }
     last_played = data[0].played_at;
-})
+});
 
 setInterval(() => {
     socket.emit("get_recent");
-}
-, 10000);
+}, 10000);
 
 setInterval(() => {
     socket.emit("get_playing");
-}
-, 2500);
+}, 2500);
 
 socket.emit("get_all_recent");
 socket.emit("get_playing");
 
 //Sleep function
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
